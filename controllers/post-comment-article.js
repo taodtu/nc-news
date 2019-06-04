61dd9f7305d4e9672eb5edbe634d23e71d8dd773
taodtu <taodtu@gmail.com>
2019-06-04 21:01:56 +0100

    error handleling for article route post method done

--- controllers/post-comment-article.js
+++ controllers/post-comment-article.js
@@ -1,9 +1,9 @@
 const { insertCommentToArticle } = require('../models/inser-comment-article');
 exports.postCommentToArticle = async (req, res, next) => {
  try {
+  if (Object.keys(req.body).length > 2 || !req.body.username || !req.body.body) await Promise.reject({ status: 400, msg: 'Wrong Update Input' })
   const comment = await insertCommentToArticle(req.params, req.body);
-  if (!req.body.username || !req.body.body) await Promise.reject({ status: 400, msg: 'Wrong Update Input' })
-  res.status(200).send({ comment });
+  res.status(201).send({ comment });
  } catch (err) {
   next(err)
  }
--- spec/articles-post.spec.js
+++ spec/articles-post.spec.js
@@ -3,55 +3,74 @@ const request = require('supertest');
 const app = require('../app');
 
 exports.testArticlesPost = () => {
- describe('/articles/:article_id/comments', () => {
-  it('POST status:200, and return the comment with proper keys', () => {
-   return request(app)
-    .post('/api/articles/1/comments')
-    .send({
-     username: 'butter_bridge',
-     body: 'I want to become a pornstar'
-    })
-    .expect(200)
-    .then(({ body }) => {
-     expect(body.comment).to.be.an('object');
-     expect(body.comment).to.include(
-      {
-       author: 'butter_bridge',
-       comment_id: 19,
-       votes: 0,
-       article_id: 1,
-       body: 'I want to become a pornstar'
-      }
-     );
+  describe.only('/articles/:article_id/comments', () => {
+    it('POST status:201, and return the comment with proper keys', () => {
+      return request(app)
+        .post('/api/articles/1/comments')
+        .send({
+          username: 'butter_bridge',
+          body: 'I want to become a star'
+        })
+        .expect(201)
+        .then(({ body }) => {
+          expect(body.comment).to.be.an('object');
+          expect(body.comment).to.include(
+            {
+              author: 'butter_bridge',
+              comment_id: 19,
+              votes: 0,
+              article_id: 1,
+              body: 'I want to become a star'
+            }
+          );
+        });
+    });
+    it('POST for an invalid article_id - status:400 and error message', () => {
+      return request(app)
+        .post('/api/articles/porn/comments')
+        .send({
+          username: 'butter_bridge',
+          body: 'I want to become a star'
+        })
+        .expect(400)
+        .then(({ body }) => {
+          expect(body.msg).to.equal('Bad Request');
+        });
+    });
+    it('POST for a non-exist article_id - status:400 and error message', () => {
+      return request(app)
+        .post('/api/articles/1123/comments')
+        .send({
+          username: 'butter_bridge',
+          body: 'I want to become a star'
+        })
+        .expect(400)
+        .then(({ body }) => {
+          expect(body.msg).to.equal('Bad Request');
+        });
+    });
+    it('POST for an invalid body key - status:400 and error message', () => {
+      return request(app)
+        .post('/api/articles/1/comments')
+        .send({ article_id: 'star' })
+        .expect(400)
+        .then(({ body }) => {
+          expect(body.msg).to.equal('Wrong Update Input');
+        });
+    });
+    it('POST for multiple keys - status:400 and error message', () => {
+      return request(app)
+        .post('/api/articles/1/comments')
+        .send({
+          article_id: 'star',
+          username: 'butter_bridge',
+          body: 'I want to become a star'
+        })
+        .expect(400)
+        .then(({ body }) => {
+          expect(body.msg).to.equal('Wrong Update Input');
+        });
     });
   });
-  it('POST for an invalid article_id - status:400 and error message', () => {
-   return request(app)
-    .post('/api/articles/porn/comments')
-    .send({ article_id: 'porn' })
-    .expect(400)
-    .then(({ body }) => {
-     expect(body.msg).to.equal('Bad Request');
-    });
-  });
-  it('POST for a non-exist article_id - status:400 and error message', () => {
-   return request(app)
-    .post('/api/articles/1123/comments')
-    .send({ article_id: 1123 })
-    .expect(400)
-    .then(({ body }) => {
-     expect(body.msg).to.equal('Bad Request');
-    });
-  });
-  it('POST for an invalid body key - status:400 and error message', () => {
-   return request(app)
-    .post('/api/articles/1/comments')
-    .send({ article_id: 'porn' })
-    .expect(400)
-    .then(({ body }) => {
-     expect(body.msg).to.equal('Wrong Update Input');
-    });
-  });
- });
 }
 
