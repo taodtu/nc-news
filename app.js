77cb9d8cf6926ef1f72c527359411228ca03bf5c
taodtu <taodtu@gmail.com>
2019-06-24 09:36:32 +0100

    install cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const apiRouter = require('./routes/api');
 const { handlePsqlErrors, handleCustomErrors, routeNotFound, handle500 } = require('./errors');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res, next) => {
  res.status(200).send({ msg: "welcome to the nc-news website, please explore by going to /api route" });
